8c9ff8733e08c204e0c2c0b1201ed1ea01d00b32
Franz Silva <franzsilvasky91@hotmail.com>
2016-08-04 03:41:46 -0300

    arreglado view formulario

--- application/config/database.php
+++ application/config/database.php
@@ -76,15 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-
 	'username' => '',
 	'password' => '',
-	'database' => 'facturacion',
-
-	'username' => 'root',
-	'password' => '',
 	'database' => '',
-
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
--- application/views/formulario.php
+++ application/views/formulario.php
@@ -9,26 +9,6 @@ $(function(){
 		$(parent).remove();
 	});
 });
-
-
-$(function renderRowCounter() {
-
-    var rowCount = [25, 50, 100];
-    var first = rowCount[0];
-    var items = '';
-
-    $("agregarProducto").on('click').each(rowCount, function(i,v) {
-        items += "<li><a href=\"#\">" + v + "</a></li>";
-    });
-
-    return  "<div class=\"btn-group btn-group-sm\">" +
-            "<button type=\"button\" data-toggle=\"dropdown\" class=\"btn btn-default dropdown-toggle\">" + first + " <span class=\"caret\"></span></button>" +
-            "<ul class=\"dropdown-menu\">" + items +
-            "</ul>" +
-            "</div> ";
-});
-
-
 </script>
 <div style ="margin: 0 auto; display: block; height: 520px; width: 900px; background-color: black">
     <div style ="margin: 0 auto; border-color: blue; display: block; height: 20px; width: 900px; background-color:silver">CREAR FACTURA</div>
@@ -36,20 +16,17 @@ $(function renderRowCounter() {
           <div style ="border-color: red;
                 //border-style: solid;
                 display: block;
-                height: 50%;
+                height: 35%;
                 width: 100%;">
-
                     <div style ="border-color: green;
                           //border-style: solid;
                           display: block;
                           height: 100%;
                           width: 30%;
                           float: left;">
-
                           <div class="form-group " style="margin:20px 20px;">
                             <label for="Nfactura">Numero de Factura</label>
                             <input style="width:70%" type="text" class="form-control" id="NumeroDeFactura" placeholder="#Factura" required="campo necesario">
-
                           </div>
                           <div class="form-group" style="margin:20px 20px;">
                             <label for="fecha de factura">Fecha de Factura</label>
@@ -79,18 +56,14 @@ $(function renderRowCounter() {
                           height: 100%;
                           width: 25%;
                           float: left;" >
-
-                          <a href="<?=base_url();?>menuController/impresionfactura" >  <button  style = "margin:40px 40px;" id="crearFactura" type="button" class="btn btn-success">crear factura</button><br></a>
-
-
-                            <button style = "margin:0 40px;" id="cancelar" type="button" class="btn btn-danger">Cancelar</button>
+                          <a href="<?=base_url();?>menuController/impresionfactura" >  <button  style = "margin:40px 40px;" id="crearFactura" type="button" class="btn btn-success">crear factura</button></a>
+                          <button style = "margin:0 40px;" id="cancelar" type="button" class="btn btn-danger">Cancelar</button>
                     </div>
           </div>
-
           <div style ="border-color: blue;
                 //border-style: solid;
                 display: block;
-                height: 50%;
+                height: 65%;
                 width: 100%;">
                 <div class="table-responsive">
                   <table class="table table-hover table-responsive" id="tabla">
@@ -101,40 +74,14 @@ $(function renderRowCounter() {
                         <th>P.U.</th>
                         <th>Dto (%)</th>
                         <th>Iva(%)</th>
-                        <th>impuesto 2</th>
+                        <th>imp 2</th>
                         <th>Subtotal</th>
                         <th></th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr class="fila-base">
-
-                        <td><input type="text" class="form-control" /></td>
-
-                        <td><div class="dropdown">
-                            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" aria_expanded="true">Productos<span class="caret"></span></button>
-                            <ul class="dropdown-menu">
-                                <li>Prod 1</li>
-                                <li>Prod 2</li>
-                                <li>Prod 3</li>
-                            </ul>
-                            </div></td>
-
-                        <td><input type="number" class="form-control" /></td>
-                        <td>10</td>
-                        <td>0</td>
-                        <td>
-                          <select >
-                            <option >iva</option>
-                            <option >13%</option>
-                            <option >14%</option>
-                          </select>
-                        </td>
-                        <td>0</td>
-                        <td>0</td>
-                        <td class="eliminar"><button  id="eliminarfila" type="button" class="btn btn-danger">x</button></td>
-                      </tr>
-											<tr>
+												<td><input type="text" class="form-control" /></td>
                         <td><input type="text" class="form-control" /></td>
                         <td><input type="number" class="form-control" /></td>
                         <td>10</td>
@@ -153,10 +100,7 @@ $(function renderRowCounter() {
                     </tbody>
                   </table>
               </div>
-
-              <button class="btn btn-success" type="button" id="agregarProducto" value="Agregar Producto" ><span class="glyphicon glyphicon-plus"> Agregar Producto</span></button>
-
-             
+              <button class="btn btn-success"  style = "margin:0 40px;" type="button" id="agregarProducto" value="Agregar Producto" ><span class="glyphicon glyphicon-plus"> Agregar Producto</span></button>
 
           </div>
     </from>
